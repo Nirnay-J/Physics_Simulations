13cd1f8dd0ec0364c031d4a2f806680c3c54c003
Nirnay Jaiswal <26154282-NirnayJaiswal@users.noreply.replit.com>
2024-04-02 06:18:28 +0000

    Created a simple pendulum

---
+++ pendulum.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
+
+l = 1
+g = 9.8
+amplitude_theta = np.pi/4 #at 45 degrees
+phase = np.pi/4
+
+def position(t):
+	theta = amplitude_theta*np.sin(np.sqrt(g/l)*t+phase)
+	pos = [l*np.sin(theta),-l*np.cos(theta)]
+	return pos
+
+t_range = np.linspace(0,10,1000)
+positions = np.array([position(t) for t in t_range])
+x_pos = [i[0] for i in positions]
+y_pos = [i[1] for I in positions]
+
+plt.scatter(x_pos,y_pos)
+plt.show()
