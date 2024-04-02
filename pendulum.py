45fe9a3c19b07e370eda51ce647eb53a8055f76c
Nirnay Jaiswal <26154282-NirnayJaiswal@users.noreply.replit.com>
2024-04-02 06:20:07 +0000

    Fixed bug

--- pendulum.py
+++ pendulum.py
@@ -15,7 +15,7 @@ def position(t):
 t_range = np.linspace(0,10,1000)
 positions = np.array([position(t) for t in t_range])
 x_pos = [i[0] for i in positions]
-y_pos = [i[1] for I in positions]
+y_pos = [i[1] for i in positions]
 
 plt.scatter(x_pos,y_pos)
 plt.show()
