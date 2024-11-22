54c163c8e7c7b0c5f45312f284424fe617936766
Nirnay Jaiswal <105607638+Nirnay-J@users.noreply.github.com>
2024-11-22 23:28:40 +0530

    Update pendulum.py

--- pendulum.py
+++ pendulum.py
@@ -20,6 +20,5 @@ positions = np.array([position(t) for t in t_range])
 x_pos = [i[0] for i in positions]
 y_pos = [i[1] for i in positions]
 
-# Creating platform for animation of pendulum
 plt.scatter(x_pos,y_pos)
 plt.show()
